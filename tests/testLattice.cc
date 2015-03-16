00a7910e456eb1f1eb48a2891158fd0882a40837
og113 <og113@imperial.ac.uk>
2015-03-16 09:51:38 +0000

    commit to pull

---
+++ tests/testLattice.cc
@@ -0,0 +1,14 @@
+/*
+	main for program to test lattice.cc
+*/
+
+#include <iostream>
+#include "lattice.h"
+
+using namespace std;
+
+int main() {
+cout << "test: " << endl;
+
+return 0;
+}
---
+++ tests/testPrint.cc
@@ -0,0 +1,14 @@
+/*
+	main for program to test print.cc
+*/
+
+#include <iostream>
+#include "print.h"
+
+using namespace std;
+
+int main() {
+cout << "test: " << endl;
+
+return 0;
+}
